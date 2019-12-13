9c4ea38fdd217da8e3eea2e0b3e5dc776a752770
Rondiele Guimaraes <rondineleg@edu.univali.br>
2019-12-13 15:19:48 -0200

    echo Statement

--- src/echo-printf.php
+++ src/echo-printf.php
@@ -10,35 +10,11 @@
 <body>
 
 <?php
+    echo "<h2> PHP is fun !</h2>";
+    echo "Hello world!<br>";
+    echo "I'm about to learn PHP ! <br>";
+    echo "This", "string","was", "made","with multiple parameters";
 
-    echo "Teste de Impressão!<br>";
-    $s="Teste de Impressão!<br>";
-    echo $s;
-    echo "S = $s";
-    
-    $n = 80;
-    printf("<p> n = %d </p>", $n);
-    printf("<p> n = %8d </p>", $n);
-    printf("<p> n = %08d </p>", $n);
-
-    $d = 80.10;
-    printf("<p> n = %8.2f </p>", $d);
-    printf("<p> n = %08.2f </p>", $d);
-    
-    $s = "Meu Texto"; 
-    printf("<p>%s: %d </p>\n",   $s, $n);
-    printf("<p>%15s:  </p>\n", $s);
-    printf("<p>%.5s:  </p>\n", $s);
-    
-    $b = true;
-    echo "<p> $b </p> ";
-    
-    echo "<br>";
-    
-    var_dump($s);
-    var_dump($n);
-    var_dump($d);
-    var_dump($b);
 
 ?>
 
