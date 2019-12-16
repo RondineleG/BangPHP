1d37425c9cf18223edf267762121f0af974c5284
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-16 11:45:46 -0300

    ssignment Operators

--- src/operators.php
+++ src/operators.php
@@ -17,7 +17,7 @@
     $y = 2;
     print "The value of x is : $x <br>";
     print "The value of y is : $y <br>";
-    echo"<br><br>";
+    echo "<br><br>";
 
     /* Operator   Name                 Example               Result 
         +         Addition             $x + $y = 52          Sum of $x and $y
@@ -27,31 +27,64 @@
         %         Modulus              $x % $y = 0           Remainde of $x divided by $y
         **        Exponentiation       $x ** $y = 2500       Result of rainsing of $x to the $y
     */
-    
-    
-   print " The Addition is :";
-   print $x + $y;  
-   echo"<br>";
 
-   print " The Subtraction is :"; 
-   print $x - $y;   
-   echo"<br>";
 
-   print " The Multiplication is :"; 
-   print $x * $y;
-   echo"<br>";  
+    print " The Addition is :";
+    print $x + $y;
+    echo "<br>";
 
-   print " The Division is :"; 
-   print $x / $y;
-   echo"<br>"; 
+    print " The Subtraction is :";
+    print $x - $y;
+    echo "<br>";
 
-   print " The Modulus is :"; 
-   print $x % $y;
-   echo"<br>"; 
+    print " The Multiplication is :";
+    print $x * $y;
+    echo "<br>";
 
-   print " The Exponentiation is :"; 
-   print $x ** $y ;
-   echo"<br>";
+    print " The Division is :";
+    print $x / $y;
+    echo "<br>";
+
+    print " The Modulus is :";
+    print $x % $y;
+    echo "<br>";
+
+    print " The Exponentiation is :";
+    print $x ** $y;
+    echo "<br>";
+
+
+    print "<h2>Assignment Operators</h2>";
+
+
+    /* Assignment      Same As         Description 
+       x = y           x = y           The left operand gets set to he value of the expression on the right
+       x += y          x = x + y       Addition      
+       x -= y          x = x - y       Subtraction   
+       x *= y          x = x * y       Multiplication
+       x /= y          x = x / y       Division      
+       x %= y          x =  x % y      Modulus       
+    */
+
+    print " The Addition is :";
+    print $x += $y;
+    echo "<br>";
+
+    print " The Subtraction is :";
+    print $x -= $y;
+    echo "<br>";
+
+    print " The Multiplication is :";
+    print $x *= $y;
+    echo "<br>";
+
+    print " The Division is :";
+    print $x /= $y;
+    echo "<br>";
+
+    print " The Modulus is :";
+    print $x %= $y;
+    echo "<br>";
 
 
 
