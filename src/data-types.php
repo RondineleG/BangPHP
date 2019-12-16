c2db9d4c37ba04b60806a39d7e97a6609d1e3aab
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-16 09:29:46 -0300

    Object

--- src/data-types.php
+++ src/data-types.php
@@ -10,54 +10,90 @@
 
 <body>
     <?php
-       echo "<h2>String</h2>";
+    echo "<h2>String</h2>";
 
-       # this is a declaration of string 
+    # this is a declaration of string 
 
-       $fistName = "Rondiele";
-       $lastName = "Guimarães";
-       $aboutMe = "<p>I'm studying, i really this area,<br>
+    $fistName = "Rondiele";
+    $lastName = "Guimarães";
+    $aboutMe = "<p>I'm studying, i really this area,<br>
         i don't understand everything very well. But my persistence is very
         high,<br> so let's write a lot of code.</p>
         ";
-       echo $aboutMe;
-       print "Author : ";
-       //concatenations strings using "."
-       echo $fistName."".$lastName; 
-       print "<br>";
-   
-
-       echo "<h2>Integer</h2>";
-
-       # this is a declaration of integer
-       
-       $age = 27;
-       $numberPhone =1245412;
-
-       var_dump($age);
+    echo $aboutMe;
+    print "Author : ";
+    //concatenations strings using "."
+    echo $fistName . "" . $lastName;
+    print "<br>";
 
 
-       echo "<h2>Float</h2>";
+    echo "<h2>Integer</h2>";
 
-       # this is a declaration of float
+    # this is a declaration of integer
 
-       $height = 1.66;
-       $price = 6.88;
+    $age = 27;
+    $numberPhone = 1245412;
 
-       var_dump($height);
-
-       echo "<h2>Boolean</h2>";
-
-       # this is a declaration of float
-
-       $isLive = true;
-       $isDead = false;
-
-       var_dump($isDead);
+    var_dump($age);
 
 
+    echo "<h2>Float</h2>";
 
-      
+    # this is a declaration of float
+
+    $height = 1.66;
+    $price = 6.88;
+
+    var_dump($height);
+
+    echo "<h2>Boolean</h2>";
+
+    # this is a declaration of boolean
+
+    $isLive = true;
+    $isDead = false;
+
+    var_dump($isDead);
+
+
+    echo "<h2>Array</h2>";
+
+    # this is a declaration of array
+
+    $cars = array("Fiesta", "Gol", "Uno");
+
+    var_dump($cars);
+
+
+    echo "<h2>Object</h2>";
+
+    # this is a declaration of object
+
+    class Car
+    {
+        function Car()
+        {
+            $this->maker = "Ford";
+            $this->category = "Compact";
+            $this->color = "Red";
+
+        }
+    }
+    $ka = new Car();
+    print "The maker of car is : ";
+    echo $ka->maker;
+    print "<br>";
+    print "The category of car is : ";
+    echo $ka->category;
+    print "<br>";
+    print "The color of car is : ";
+    echo $ka->color;
+
+
+
+
+
+
 
     ?>
 </body>
