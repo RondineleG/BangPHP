066774b293fe8fd222acddb415862e93e2b414d2
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-16 11:10:22 -0300

    Constant

---
+++ src/constants.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP - Constants</title>
+</head>
+
+<body>
+    <?php
+    print "<h2>Constant</h2>";
+
+    define("HELLO", "Welcome to w3schoorls.com !");
+    print HELLO;
+    echo "<br>";
+
+    ?>
+</body>
+
+</html>
