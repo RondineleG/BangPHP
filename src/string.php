2116abb0f4cb3e02ce0b67a0a30989e4c9762e74
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-16 09:44:33 -0300

    strlen()

---
+++ src/string.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP - Strings Functions</title>
+</head>
+
+<body>
+    <?php 
+       print "<h2> Strings Functions</h2>";
+
+       # strlen() - return the lenght of a string
+
+       echo strlen("Hello, Rondinle Guimaraes");
+    
+    ?>
+</body>
+
+</html>
