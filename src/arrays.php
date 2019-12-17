d748acd5c4609afb0a53f6246c203754d79be279
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-17 10:34:39 -0300

    Multidimensional Arrays

--- src/arrays.php
+++ src/arrays.php
@@ -36,13 +36,23 @@
     print "Peter is " . $age['Peter'] . " years old.";
 
     print "<h2>Loop through Associative Arrays</h2>";
-    foreach($age as $x => $x_values)
-    {
-        print"Key=" . $x .", Value=" . $x_values;
-        print"<br>";
-
+    foreach ($age as $x => $x_values) {
+        print "Key=" . $x . ", Value=" . $x_values;
+        print "<br>";
     }
 
+    print "<h2>Multidimensional Arrays</h2>";
+    $cars = array(
+        array("Volvo", 22, 18),
+        array("BMW", 15, 13),
+        array("Saab", 5, 2),
+        array("Land Rover", 17, 15)
+    );
+
+    echo $cars[0][0] . ": In stock: " . $cars[0][1] . ", sold: " . $cars[0][2] . ".<br>";
+    echo $cars[1][0] . ": In stock: " . $cars[1][1] . ", sold: " . $cars[1][2] . ".<br>";
+    echo $cars[2][0] . ": In stock: " . $cars[2][1] . ", sold: " . $cars[2][2] . ".<br>";
+    echo $cars[3][0] . ": In stock: " . $cars[3][1] . ", sold: " . $cars[3][2] . ".<br>";
     ?>
 </body>
 
