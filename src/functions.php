99e03b07992c463afc1b9875a91acaca4049eb62
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-17 09:32:19 -0300

    functions

---
+++ src/functions.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP - Functions</title>
+</head>
+
+<body>
+    <?php
+
+    print "<h2>Functions</h2>";
+
+    function writeMessage()
+    {
+        print("Hello World");
+    }
+    
+    // call the function
+    writeMessage();
+    ?>
+</body>
+
+</html>
