f1ef945a48939041f3b1cb191272b92e5d22244c
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-17 09:18:40 -0300

    for loop

---
+++ src/for.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP - Loop For</title>
+</head>
+
+<body>
+    <?php
+     print "<h2>for loop </h2>";
+
+     for($x =0; $x <15; $x++)
+     {
+        print "The number is : $x<br>";
+     }
+     print "<br><br>";     
+    
+    ?>
+</body>
+
+</html>
