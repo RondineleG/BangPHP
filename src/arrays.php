61da9926be0a22a30bc723d2b2166e1559f31e02
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-17 10:28:04 -0300

    Associative Arrays

--- src/arrays.php
+++ src/arrays.php
@@ -10,26 +10,30 @@
 
 <body>
     <?php
-    print"<h2>Arrays</h2>";
+    print "<h2>Arrays</h2>";
 
-    $cars = array ("uno","gol","palio");
+    $cars = array("uno", "gol", "palio");
 
-    print"I like " .$cars[0] . " , ".$cars[1]." and ".$cars[2].".";
-    
-    print"<br>";
+    print "I like " . $cars[0] . " , " . $cars[1] . " and " . $cars[2] . ".";
+
+    print "<br>";
     // lenght of array
     print count($cars);
 
 
-    print"<h2>Loop through Indexed Arrays</h2>";
-    
+    print "<h2>Loop through Indexed Arrays</h2>";
+
     $carrLenght = count($cars);
 
-    for($x = 0; $x < $carrLenght; $x++)
-    {
-       print $cars[$x];
-       print "<br>";
+    for ($x = 0; $x < $carrLenght; $x++) {
+        print $cars[$x];
+        print "<br>";
     }
+
+    print "<h2>Associative Arrays</h2>";
+
+    $age = array("Peter" => "35", "Ben" => "37", "Joe" => "43");
+    print "Peter is " . $age['Peter'] . " years old.";
 
     ?>
 </body>
