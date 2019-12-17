e364124dd19930f2e8a0f654fe87d65f7214a580
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-17 08:22:57 -0300

    If conditional

---
+++ src/conditional.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP</title>
+</head>
+
+<body>
+    <?php
+       
+       print"<h2>The if statement</h2>";
+
+
+       $hour = date("H");
+
+       if($hour <"18")
+       {
+           print("Have a good day!");
+
+       }
+
+       print"<h2>The if..else statement</h2>";
+
+       //Conditional with logical operator
+       if($hour >"9" && $hour < "18")
+       {
+           print("Have a good day!");
+
+       }
+       else
+       {
+        print("Have a good night!");
+       }
+
+       
+       print"<h2>The if..elseif..else statement</h2>";
+
+       if($hour >"6" && $hour<"12")
+       {
+           print "Have a good morning";
+       }
+       elseif($hour >"12" && $hour <"18")
+           {
+            print "Have a good aftenoon";
+           }
+       else
+       {
+        print "Have a good nigh";
+       }
+
+
+    ?>
+</body>
+
+</html>
