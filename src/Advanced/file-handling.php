1dff8aa3764cb15af2c23614b32d36ddb530ff2f
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-18 12:50:23 -0300

    File Handling

---
+++ src/Advanced/file-handling.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP - File Handling</title>
+</head>
+
+<body>
+    <?php
+    print "<h2>readfile() function</h2>";
+
+
+    ?>
+</body>
+
+</html>
