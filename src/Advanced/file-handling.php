bace5b7a6b9f4c9428c5fb43ce3fdb549b2bffea
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-18 13:49:20 -0300

    Creating , reading files txt and json

--- src/Advanced/file-handling.php
+++ src/Advanced/file-handling.php
@@ -20,7 +20,7 @@
     //read
     print fread($myFile, filesize("webdictionary.txt"));
     //close
-    fclose($myFile);    
+    fclose($myFile);
 
     print "<h2>Read Single line fgets()</h2>";
 
@@ -29,59 +29,84 @@
     //read one line
     print fgets($myFile, filesize("webdictionary.txt"));
     //close
-    fclose($myFile);  
-  
+    fclose($myFile);
+
     print "<h2>Check End-Of-Lif feof()</h2>";
 
     //open
     $myFile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
     //read one line
-   
-    while(!feof($myFile))
-    {
-        print fgets($myFile) ."<br>";
 
+    while (!feof($myFile)) {
+        print fgets($myFile) . "<br>";
     }
     //close
-    fclose($myFile);  
-    
+    fclose($myFile);
+
     print "<h2>Check single caracter fgetc()</h2>";
 
     //open
     $myFile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
     //read one line
-   
-    while(!feof($myFile))
-    {
-        print fgetc($myFile) ."<br>";
 
+    while (!feof($myFile)) {
+        print fgetc($myFile) . "<br>";
     }
     //close
-    fclose($myFile); 
+    fclose($myFile);
 
-    print "<h2>Write to file - fwrite()</h2>";
+    print "<h2>fwrite()</h2>";
+
+    print "<h2>Write to file .txt </h2>";
     //open
     $myFile = fopen("newfile.txt", "w") or die("Unable to open file!");
     //write
-    $txt = " Rondinele " ."\n" ;
+    print $txt = " Rondinele Guimaraes" . "\n";
     fwrite($myFile, $txt);
-    print"<br>";
-    $txt = " Beatriz "."\n";
+    print $txt = " Beatriz Oliveira" . "\n";
     fwrite($myFile, $txt);
     //close
-    fclose($myFile);  
+    fclose($myFile);
 
-      //open
-    $myFile = fopen("newfile.txt", "r") or die("Unable to open file!");
-    //read one line
+    print "<h2>Write to file .json </h2>";
+
+    //open
+    $newFile = fopen("newfile.json", "w") or die("Unable to open file!");
+    //write
+    print $txt = " Gabriela Guimaraes " . "\n";
+    fwrite($newFile, $txt);
+    print $txt = " Janaina Oliveira " . "\n";
+    fwrite($newFile, $txt);
+    //close
+    fclose($newFile);
+
    
-    while(!feof($myFile))
-    {
-        print fgets($myFile) ."<br>";
+    print "<h2>Read json()</h2>";
 
+    //open
+    $newFile = fopen("newfile.json", "r") or die("Unable to open file!");
+    //read one line
+
+    while (!feof($newFile)) {
+        print fgets($newFile) . "<br>";
     }
     //close
-    fclose($myFile);  
+    fclose($newFile);
+
+
+    print "<h2>Read txt()</h2>";
+
+    //open
+    $myFile = fopen("newfile.txt", "r") or die("Unable to open file!");
+    //read one line
+
+    while (!feof($myFile)) {
+        print fgets($myFile) . "<br>";
+    }
+    //close
+    fclose($myFile);
+
+    
     ?>
 </body>
 
