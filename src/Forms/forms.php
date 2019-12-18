f31309e98985a958d7fc57c781d32156e205c68c
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-18 10:31:29 -0300

    Create post method

--- src/Forms/forms.php
+++ src/Forms/forms.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-<form action="#" method="POST">
+<form action="welcome.php" method="POST">
 Name : <input type="text" name="name"><br>
 E-mail: <input type="text" name="email"><br>
 <input type="submit">
---
+++ src/Forms/welcome.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP - POST and GET Method</title>
+</head>
+
+<body>
+    Welcome
+    <?php
+      print $_POST["name"]; ?><br>
+      your email is : <?php print $_POST["email"]; ?>
+</body>
+
+</html>
