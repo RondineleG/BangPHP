7bb2e8ddb99436c69cc88da0b658b2ac52d9ca8d
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-18 11:53:09 -0300

    Get a time

--- src/Advanced/date-time.php
+++ src/Advanced/date-time.php
@@ -13,6 +13,7 @@
     print"<h2>Date and Time</h2>";
 
     print"<h2>Get a date</h2>";
+
     print "Today is " . date("Y/m/d") ."<br>";
     print "The year is " . date("Y") ."<br>";
     print "The month is " . date("m") ."<br>";
@@ -20,6 +21,15 @@
     print "Today is " . date("d") ."<br>";
     print "Today is " . date("18") ."<br>";
 
+    print"<h2>Get a time</h2>";
+    
+    print" The time is : " . date("h:i:sa")."<br>";
+    print" The hour is : " . date("h")."<br>";
+    print" The hour and minutes is : " . date("h:i")."<br>";
+    print"Only minutes : " . date("i")."<br>";
+    print"Only seconds : " . date("s")."<br>";
+   
+    
 
     ?>
 </body>
