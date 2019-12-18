55c32e117b2ce0d5c0e9826a119556975f81da91
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-18 11:52:18 -0300

    Get a date

---
+++ src/Advanced/date-time.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP -  Date and Time</title>
+</head>
+
+<body>
+    <?php
+    print"<h2>Date and Time</h2>";
+
+    print"<h2>Get a date</h2>";
+    print "Today is " . date("Y/m/d") ."<br>";
+    print "The year is " . date("Y") ."<br>";
+    print "The month is " . date("m") ."<br>";
+    print "The short date is " . date("m/y") ."<br>";
+    print "Today is " . date("d") ."<br>";
+    print "Today is " . date("18") ."<br>";
+
+
+    ?>
+</body>
+
+</html>
