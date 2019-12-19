8fb43c567bf2750c2cf54e31719450731b435b1e
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-19 08:50:13 -0300

    script upload files

--- src/Advanced/file-upload.php
+++ src/Advanced/file-upload.php
@@ -9,7 +9,7 @@
 </head>
 
 <body>
-    <form action="upload.php" method="POST" enctype="multipart/form-data" >
+    <form action="upload-script.php" method="POST" enctype="multipart/form-data" >
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload">
     <input type="submit" value="Upload Image" name="submit">    
---
+++ src/Advanced/upload-script.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP - Script to upload file</title>
+</head>
+
+<body>
+    <?php
+      $targetDirectory ="uploads/";
+      $targetFile = $targetDirectory . basename($_FILES['fileUpload']['name']);
+      
+      $uploadOK =1;
+
+      $imageFileType = strtolower(pathinfo($targetDirectory, PATHINFO_EXTENSION));
+
+      //check if image  file is a actual image or fake image
+
+      if(isset($_POST['submit']))
+      {
+          $check = getimagesize($_FILES["fileToUpload"]['tmpName']);
+
+          if($check !== false)
+          {
+              print "file is a image - " . $check["mime"].".";
+              $uploadOK = 1;
+          }
+          else
+          {
+              print "File is not a image";
+              $uploadOK = 0;
+          }
+
+      }
+
+
+    ?>
+</body>
+
+</html>
