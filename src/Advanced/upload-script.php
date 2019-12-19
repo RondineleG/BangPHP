a01ca9eee8d52b9c1d36e6ae1d6a2200f3586a92
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-19 09:32:52 -0300

    Upload of images  working

--- src/Advanced/upload-script.php
+++ src/Advanced/upload-script.php
@@ -12,33 +12,35 @@
     <?php
     
       $targetDirectory ="uploads/";
-      $targetFile = $targetDirectory . basename($_FILES['fileUpload']['name']);
+      $targetFile = $targetDirectory . basename($_FILES['fileToUpload']
+      ['name']);
       
       $uploadOK =1;
 
-      $imageFileType = strtolower(pathinfo($targetDirectory, PATHINFO_EXTENSION));
+      $imageFileType = strtolower(pathinfo($targetFile, 
+      PATHINFO_EXTENSION));
 
       //check if image  file is a actual image or fake image
 
       if(isset($_POST['submit']))
       {
-          $check = getimagesize($_FILES["fileToUpload"]['tmpName']);
+          $check = getimagesize($_FILES["fileToUpload"]['tmp_name']);
 
           if($check !== false)
           {
-              print "file is a image - " . $check["mime"].".";
+              print "file is a image - " . $check["mime"].".<br>";
               $uploadOK = 1;
           }
           else
           {
-              print "File is not a image";
+              print "File is not a image<br>";
               $uploadOK = 0;
           }
         
           // check if file aready exists
         if(file_exists($targetFile))
         {
-            print "File aready exists!";
+            print "\nFile aready exists!<br>";
             $uploadOK = 0;
         }
 
@@ -46,7 +48,7 @@
 
          if($_FILES["fileToUpload"]["size"] > 5000000)
          {
-             print "Your file is too large";
+             print "Your file is too large<br>";
              $uploadOK = 0;
          }
 
@@ -54,26 +56,26 @@
 
          if($imageFileType !="jpg" && $imageFileType !="png" && $imageFileType !="jpeg" && $imageFileType !="gif")
          {
-             print "Only files formats supported, jpg, png, jpeg and gif";
+             print "Only files formats supported, jpg, png, jpeg and gif <br>";
              $uploadOK = 0;
          }
 
          // check is $uploadOK is set to 0 by an error
          if($uploadOK ==0)
          {
-            print "Your file was not uploaded";
+            print "Your file was not uploaded <br>";
          }
          // check if everythinh is ok and try to upload file
          else
          {  
-             if(move_uploaded_file($_FILES["fileToUpload"]["tmpName"], $targetFile))
+             if(move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile))
              {
                  print "The file " . basename($_FILES["fileToUpload"]["name"]).
-                 "has been uploaded";
+                 "has been uploaded <br>";
              }
              else
              {
-                print "Error to upload";
+                print "Error to upload <br>";
              }
 
          }
