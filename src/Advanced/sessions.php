d65e21915b72bf30c0a5b0bdf0dafe39e1f21133
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-19 11:06:48 -0300

    modify session

--- src/Advanced/sessions.php
+++ src/Advanced/sessions.php
@@ -14,9 +14,15 @@ session_start();
 
 <body>
     <?php
+
     //set session variables
     print "Favorite color is " . $_SESSION["favcolor"] = "green" . "<br>";
-    print "Favorite animal is " . $_SESSION["favanimal"] = "dog";
+
+    //modify
+    print "Favorite color is " . $_SESSION["favcolor"] = "red" . "<br>";
+    print "Favorite animal is " . $_SESSION["favanimal"] = "dog<br>";
+    print "Print session <br>";
+    print_r($_SESSION);
     ?>
 </body>
 
