7e1ebeb0a3898818b58db5c713d8362c96740100
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-20 10:12:57 -0300

    update index with a litle css

--- index.php
+++ index.php
@@ -6,12 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Learning PHP</title>
+    <style>
+       h1{
+           text-align: center;
+           padding-top: 100px;
+           color: green;
+       }
+    
+    </style>
 </head>
 
 <body>
     <?php
-
+      print("<h1>Learning PHP With W3Schools</h1>");
     ?>
+    
 </body>
 
 </html>
