45130f4f2a014051ceb987374039b1c1e516db9f
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-20 11:37:13 -0300

    Creating connection

---
+++ src/DataBase/connection.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Learning PHP - Connection to MySQL</title>
+</head>
+
+<body>
+     <?php
+
+     print("<h2><b>Connection to database</h2>");
+
+     $servername = "localhost";
+     $username = "username";
+     $password = "password";
+
+     //create connection
+     $connection = new mysqli($servername,$username,$password);
+     
+     //check connection
+     if($connection -> connect_error)
+     {
+         die("Connection failed: " . $connection->connect_error);
+
+     }
+     echo"Connected successfully";
+     ?>
+ 
+</body>
+
+</html>
