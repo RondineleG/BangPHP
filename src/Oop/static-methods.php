37f0935a82088170a6f193aab19e6a70627a3e47
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-20 09:59:35 -0300

    more static method

--- src/Oop/static-methods.php
+++ src/Oop/static-methods.php
@@ -15,21 +15,43 @@
     {
         public static function Welcome()
         {
-            echo"Hello World!";
+            echo "Hello World!";
         }
 
         public function __construct()
         {
-           self::Welcome();            
+            self::Welcome();
         }
     }
+    class SomeOtherClass
+    {
+        public function message()
+        {
+            greeting::Welcome();
+        }
+    }
+    // Call static method
 
+    greeting::Welcome();
+    print "<br>";
+    new greeting();
 
-        // Call static method
-
-        greeting::Welcome();
-        print"<br>";
-        new greeting();
+    class domain {
+        protected static function getWebsiteName() {
+          return "W3Schools.com";
+        }
+      }
+      
+      class domainW3 extends domain {
+        public $websiteName;
+        public function __construct() {
+          $this->websiteName = parent::getWebsiteName();
+        }
+      }
+      print"<br>";
+      $domainW3 = new domainW3;
+      echo $domainW3 -> websiteName;
+    
     ?>
 </body>
 
