40b5cf53a357ae4b56813006b17f3eee5c797664
Rondinele Guimarães <rondineleg@edu.univali.br>
2019-12-23 09:02:26 -0300

    connection with PDO

--- src/DataBase/create-table.php
+++ src/DataBase/create-table.php
@@ -47,19 +47,46 @@
     // Create connection MySQLi Procedural
     $connection = mysqli_connect($severname, $username, $password, $dbname);
 
-      if(!$connection)
-      { 
-        printf("Can't connect to localhost. Error: %s\n", mysqli_connect_error());}
+    if (!$connection) {
+        printf("Can't connect to localhost. Error: %s\n", mysqli_connect_error());
+    }
 
-        if(mysqli_query($connection, $sql))
-        {
-            print"Table created successfully";
-        }
-        else
-        {
-            print"Error creanting table : " . mysqli_error($connection);
-        }
-        $connection->close();
+    if (mysqli_query($connection, $sql)) {
+        print "Table created successfully";
+    } else {
+        print "Error creanting table : " . mysqli_error($connection);
+    }
+    $connection->close();
+
+
+    // Create connection PDO
+
+    try {
+        $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+
+        // set the pdo error mode to excption
+
+
+        $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        //sql to create table
+        $sql = " CREATE TABLE MyGuest (
+        id int (6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        firstname VARCHAR(50) NOT NULL,
+        lastname VARCHAR (50),
+        reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+
+    )";
+
+        // use exec() bacause no result are returned
+
+    } catch (PDOException $ex) {
+
+        print $sql . "<br>" . $ex->getMessage();
+    }
+
+    $connection = null;
+
     ?>
 </body>
 
