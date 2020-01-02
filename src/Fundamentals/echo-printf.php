6b97c81da3944228f8a3d61bb41b6eefdb08f42f
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-02 10:45:33 -0300

    encho and print

---
+++ src/Fundamentals/echo-printf.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<title> print and echo </title>
+</head>
+<body>
+
+<?php
+
+    echo "this is a message of echo fuction!<br>";
+ 
+    print "this is a message of print fuction!<br>";
+
+    $message = "Hello world";
+
+    print $message . "<br>";
+    echo $message . "<br>";
+
+?>
+
+</body>
