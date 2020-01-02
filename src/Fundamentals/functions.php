13bc9202e73e9b55f768c743c7735fe34ad4f70c
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-02 11:13:04 -0300

    is_integer()

---
+++ src/Fundamentals/functions.php
@@ -0,0 +1,19 @@
+<html>
+
+<head>
+    <title>PHP Functions</title>
+</head>
+
+<body>
+    <?php
+
+    // is_integer();
+
+    $i = 1;
+    $b = is_integer($i);
+    echo "is_integer(i) = $b <br>";
+
+    ?>
+</body>
+
+</html>
