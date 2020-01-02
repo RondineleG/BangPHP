d16f9c1685381f5d1a81441e9f9063c63d54c895
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-02 12:20:46 -0300

    ceil() an floor()

--- src/Fundamentals/float.php
+++ src/Fundamentals/float.php
@@ -32,6 +32,7 @@
     printf("<p> r1 = %f </p>\n", $r1);
     printf("<p> r2 = %f </p>\n", $r2);
 
+
     // ceil() e floor()
     $r = 15.366;
     $r0 = ceil($r);
@@ -40,27 +41,6 @@
     printf("<p> ceil  r0 = %f </p>\n", $r0);
     printf("<p> floor r1 = %f </p>\n", $r1);
 
-    // intval()
-    $r = 15.366;
-    $i = intval(15.366);
-    $b = is_float($r);
-    echo "is_float( $r ) = $b <br>";
-    $b = is_integer($i);
-    echo "is_integer( $i ) = $b <br>";
-
-    // pow
-    $r = pow(2, 10);
-    printf("<p> 2^10 = %10.4f </p>\n", $r);
-    $r = pow(2.1, 9);
-    printf("<p> 2.1^9 = %10.4f </p>\n", $r);
-
-    // rand
-    $r = rand();
-    printf("<p> rand = %10.4f </p>\n", $r);
-    printf("<p> rand = %10.4f </p>\n", rand());
-    printf("<p> rand = %10.4f </p>\n", rand());
-    printf("<p> rand 1-10   = %10.4f </p>\n", rand(1, 10));
-    printf("<p> rand 10-100 = %10.4f </p>\n", rand(10, 100));
     ?>
 </body>
 
