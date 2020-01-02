7768517a539b1ef9ced11e0245372372d51849fb
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-02 13:02:33 -0300

    strpos()

--- src/Fundamentals/string.php
+++ src/Fundamentals/string.php
@@ -1,12 +1,14 @@
 <html>
+
 <head>
-<title>Strings </title>
+    <title>Strings </title>
 </head>
+
 <body>
-<?php
+    <?php
     $s = "Some Text";
     printf("<p>s: %s </p>\n", $s);
-    
+
     // Concatenation  using the "."
     $s = $s . ": Concatenation...";
     printf("<p>s: %s </p>\n", $s);
@@ -19,30 +21,47 @@
 
     $s = "Numero i = " . $i . "; ";
     printf("<p>s: %s </p>\n", $s);
-    
+
 
     /* Functions */
 
     // strlen() for count
     $s = "Hello world";
     $i = strlen($s);
-   printf("<p>Lengt of 's' is : %d</p>",$i);
-    echo"<hr>";
+    printf("<p>Lengt of 's' is : %d</p>", $i);
+    echo "<hr>";
 
-   // strtoupper()
-   $name = "rondinele";
-   echo "Value of name is : ". $name;
-   $u= strtoupper($name);
-   printf("<p>strtoupper of 'name' is : %s </p>",$u);
-   echo"<hr>";  
-   
-   // strtolower()
-   $name = "BEATRIZ";
-   echo "Value of name is : ". $name;
-   $u= strtolower($name);
-   printf("<p>strtolower of 'name' is : %s </p>",$u);
-   echo"<hr>";  
-   
-?>
+    // strtoupper()
+    $name = "rondinele";
+    echo "Value of name is : " . $name;
+    $u = strtoupper($name);
+    printf("<p>strtoupper of 'name' is : %s </p>", $u);
+    echo "<hr>";
+
+    // strtolower()
+    $name = "BEATRIZ";
+    echo "Value of name is : " . $name;
+    $u = strtolower($name);
+    printf("<p>strtolower of 'name' is : %s </p>", $u);
+    echo "<hr>";
+
+    // strpos()
+    $p = "Hello world";
+    $t = "da";
+    $s = strpos($p, $t);
+    if ($s == false)
+        printf("<p>Not found</p>");
+    else
+        printf("<p>strpos of p is : %d </p>", $s);
+    echo "<hr>";
+
+
+
+
+
+
+
+    ?>
 </body>
+
 </html>
