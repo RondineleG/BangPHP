9958a9272e13b5c99f125a85f32bd7e3dd3019c3
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-03 09:08:59 -0300

    array_merge()  and array_fill()

--- src/Fundamentals/array.php
+++ src/Fundamentals/array.php
@@ -30,7 +30,32 @@
       echo (' array_keys($myArray) is : ');
       var_dump($arryKeys);
 
+      echo"<hr>";
+      $Keys2 = ["Name:"=>"Rodinele", "Age:"=>"27"];
+      $arryKeys2 = array_keys($Keys2);
+      echo (' array_keys2($myArray) is : ');
+      var_dump($arryKeys2);
 
+      echo"<hr>";
+
+      //array_merge()
+
+      $array1 = array("Red","Green");
+      $array2 = array("Blue","White");
+
+      $r = array_merge($array1, $array2);
+
+      echo (' array_merge($a1, $a2) is : ');
+      var_dump($r);
+
+
+      echo"<hr>";
+
+      //array_fill()
+
+      $a1 = array_fill(0,4,"Blue");
+      echo (' array_fill(0,4,"Blue") is : ');
+      var_dump($a1);
 
 ?>
 
