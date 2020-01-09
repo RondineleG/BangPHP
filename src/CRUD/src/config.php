f85a0e19a2278297496d62fe56b1481a9e9220ef
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-09 13:26:23 -0300

    Creating config database file

---
+++ src/CRUD/src/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// Host name
+define('DB_HOST', 'den1.mysql1.gear.host');
+
+// Database name
+define('DB_NAME', 'phpcrud');
+
+// User name
+define('DB_USER', 'phpcrud');
+
+// Password
+define('DB_PASSWORD', '');
