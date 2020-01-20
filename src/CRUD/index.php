cf7b5c9acaaabe22fff93ffe09385abbc1bfc1ad
Rondinele Guimarães <rondineleg@edu.univali.br>
2020-01-20 08:13:04 -0300

    Remove folder CRUD

--- src/CRUD/index.php
+++
@@ -1,9 +0,0 @@
-<?php require_once 'config.php'; ?>
-	<?php require_once DBAPI; ?>	
-<?php
-$db = openDatabase();
-if ($db) {
-    echo '<h1>Conected!</h1>';
-} else {
-    echo '<h1>ERROR: Not connected!</h1>';
-}    ?>
